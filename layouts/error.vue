9061ba6d3cfbe2c9f9f8c9d637e6353283a66a62
liuguangqing <liuguangqing@edspay.com>
2019-04-15 11:41:50 +0800

    增加 404 page

---
+++ layouts/error.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="err_bg">
+        <div class="err_tain">
+            <h2 v-if="error.statusCode == 404">404页面不存在</h2>
+            <h2 v-else>500服务器错误</h2>
+            <nuxt-link to="/bk_front/home/all-1">返回首页</nuxt-link>
+        </div>
+
+    </div>
+</template>
+<script>
+export default {
+    props: ['error']
+}
+</script>
+<style lang="less">
+.err_bg {
+    padding: 10px;
+    width: 100%;
+    height: 100%;
+    box-sizing: border-box;
+    .err_tain {
+        padding: 10px;
+        box-sizing: border-box;
+        width: 100%;
+        height: 100%;
+        height: 100%;
+        min-height: 400px;
+        border-radius: 10px;
+        background:#fff url(http://xiangjv.top/xiangjv_404.png) no-repeat center top / 60%;
+        text-align: center;
+        padding-top: 50%;
+        color: #2e2e2e;
+        font-size: 16px;
+        font-family: cursive;
+        h2 {
+            padding: 10px;
+        }
+    }
+}
+</style>
